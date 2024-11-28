4d5be578875610e62eb50be09b656857bab936ce
Caio LacerdaDev <lacerdacaiogabriel@gmail.com>
2024-11-28 09:45:57 -0300

    feat: add initial method

--- src/routes/alunos.routes.js
+++ src/routes/alunos.routes.js
@@ -1,1 +1,21 @@
-a
+import { Router } from "express";
+
+const alunosRoutes = Router();
+
+alunosRoutes.post("/adicionar", (req, res) => {
+
+});
+
+alunosRoutes.get("/listar", (req, res) => {
+
+});
+
+alunosRoutes.put("/atualizar", (req, res) => {
+
+});
+
+alunosRoutes.delete("/remover", (req, res) => {
+
+});
+
+export default alunosRoutes;
